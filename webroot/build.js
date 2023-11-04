7669f0d712b4212d91b1d13c07bcd921ac1c4e05
Matthew Forrester <matthew.forrester@speechmarks.com>
2023-11-04 14:53:01 +0000

    feat: timing page

--- webroot/build.js
+++ webroot/build.js
@@ -415,18 +415,3 @@ getDatabase(club)
         console.error(err);
     })
 
-
-// db.serialize(() => {
-//     db.run("CREATE TABLE lorem (info TEXT)");
-//
-//     const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-//     for (let i = 0; i <Details 10; i++) {
-//         stmt.run("Ipsum " + i);
-//     }
-//     stmt.finalize();
-//
-//     db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
-//         console.log(row.id + ": " + row.info);
-//     });
-// });
-
